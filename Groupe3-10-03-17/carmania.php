0ae0ce86d5ec15a8cf4e88635a4a1a5db07e913a
Ganti7 <soso1350@hotmail.fr>
2017-03-10 16:58:51 +0100

    Add files via upload

---
+++ Groupe3-10-03-17/carmania.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+	<html class="fond">
+
+		<head>
+
+			<meta charset="UTF-8">
+
+			<link rel="stylesheet" type="text/css" href="carmania.css">
+		</head>
+
+	<body>
+		<h1 class= "titre" >Carmania</h1>
+		
+		<a href="carmania_co.html"><button class="bouton bouton1">Connexion</button></a>
+
+		<span  class= "test">
+		</span>
+			<a href="carmania_co.php"><button class="bouton_a" ><span>Achat </span></button></a>
+
+        
+
+	</body>
