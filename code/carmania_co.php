495b69faa92141832697e6c82501fcf249ed4b18
Ganti7 <soso1350@hotmail.fr>
2017-03-10 15:12:51 +0100

    Add files via upload

---
+++ code/carmania_co.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+	<html class="fond">
+
+		<head>
+
+			<meta charset="UTF-8">
+
+			<link rel="stylesheet" type="text/css" href="carmania.css">
+		</head>
+
+	<body>
+		<h1>Carmania</h1>
+		
+		Nom :  <input class="input" type="text" name="FirstName"><br>
+		Prénom : <input class="input" type="text" name="LastName"><br>
+		Adresse : <input class="input" type="text" name="LastName"><br>
+
+
+
+
+        
+
+	</body>
+
+</html>
