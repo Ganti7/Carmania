7600746a2005382a519e601faf4692aa4b345026
Ganti7 <soso1350@hotmail.fr>
2017-03-24 16:55:33 +0100

    Add files via upload

---
+++ Groupe3 24-03-2017/verif.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$lvl=(isset($_SESSION['level']))?(int) $_SESSION['level']:1;
+
+$id=(isset($_SESSION['id']))?(int) $_SESSION['id']:0;
+
+$mail=(isset($_SESSION['mail']))?$_SESSION['mail']:'';
+
+?>
