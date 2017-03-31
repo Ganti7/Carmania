ea220f657b7632fa7b19cc782a9ae5a6f12cd4c5
Akafuma <akafuma.af@gmail.com>
2017-03-31 16:24:26 +0200

    Add files via upload
    Bugfixing
    Redirection après connexion, déconnexion, inscription

---
+++ Groupe3-31-03-17/carmania_co.php
@@ -0,0 +1,105 @@
+<?php
+
+session_start();
+include("functions.php");
+include("identifiants.php");
+include("verif.php");
+include("constants.php");
+?>
+
+
+
+<!DOCTYPE html>
+
+	<html class="fond">
+
+		<head>
+
+			<meta charset="UTF-8">
+
+			<link rel="stylesheet" type="text/css" href="carmania.css">
+		</head>
+
+	<body>
+		<h1>Carmania</h1>
+		
+		
+		<?php
+		
+		if ($id!=0) erreur(ERR_IS_CO);
+		?>
+		
+		<?php
+		
+		if (!isset($_POST['mail'])) //On est dans la page de formulaire
+
+			{
+		
+			
+				echo '<form method="post" action="carmania_co.php"
+				<label for="mail">Adresse mail : </label><input class="input" type="text" name="mail"><br>
+				<label for="Password">Mot de passe :</label><input class="input" type="password" name="Password"><br>
+				<input type="submit" value="Connexion" />
+				<a href="./carmania_register.php">Pas encore inscrit ?</a>';
+		
+			}
+
+		else
+			{
+				
+			$message='';
+			//$page='';
+			if (empty($_POST['mail']) || empty($_POST['Password']) ) //Oublie d'un champ
+				{
+					$message = '<p>une erreur s\'est produite pendant votre identification.
+					Vous devez remplir tous les champs</p>
+					<p>Cliquez <a href="./carmania_co.php">ici</a> pour revenir</p>';
+				}
+			else //On check le mot de passe
+				{
+				$query=$db->prepare('SELECT adresse_mail_utilisateur, mot_de_passe , prenom_utilisateur
+				FROM Utilisateur WHERE adresse_mail_utilisateur = :mail');
+				$query->bindValue(':mail',$_POST['mail'], PDO::PARAM_STR);
+				$query->execute();
+				$data=$query->fetch();
+				
+			
+				if ($data['mot_de_passe'] ==$_POST['Password']) // Acces OK !
+					{
+					$_SESSION['mail'] = $data['adresse_mail_utilisateur'];
+					//$page = htmlspecialchars($_POST['page']);
+					$_SESSION['id'] =1;
+					
+					header("Location: carmania.php"); // redirection
+					/*
+					$message = '<p>Bienvenue '.$data['prenom_utilisateur'].', 
+					vous êtes maintenant connecté!</p>
+					<p>Cliquez <a href="'.$page.'">ici</a> 
+					pour revenir à la page précédente</p>'; 
+					*/					
+					}
+				else // Acces pas OK !
+					{
+					$message = '<p>Une erreur s\'est produite 
+					pendant votre identification.<br /> Le mot de passe ou le mail 
+					entré n\'est pas correcte.</p><p>Cliquez <a href="./carmania_co.php">ici</a> 
+					pour revenir à la page précédente
+					<br /><br />Cliquez <a href="./carmania.php">ici</a> 
+					pour revenir à la page d accueil</p>';
+					}
+    $query->CloseCursor();
+				}		
+    echo $message.'</div></body></html>';
+
+			}
+?>
+
+		
+
+
+
+        
+
+	</body>
+
+</html>
---
+++ Groupe3-31-03-17/carmania_deco.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+session_destroy();
+include("identifiants.php");
+include("verif.php");
+include("constants.php");
+include("functions.php");
+?>
+
+
+
+<!DOCTYPE html>
+
+	<html class="fond">
+
+		<head>
+
+			<meta charset="UTF-8">
+
+			<link rel="stylesheet" type="text/css" href="carmania.css">
+		</head>
+
+	<body>
+		<h1>Carmania</h1>
+		<?php
+		
+		if(!isset($page)) $page =$_SERVER["HTTP_REFERER"];
+		?>
+		
+		<?php
+			if ($id==0) erreur(ERR_IS_NOT_CO);
+		?>
+		
+		<?php
+			header("Location: carmania.php"); // redirection
+			/*
+            echo '<p>Vous êtes à présent déconnecté <br />
+            Cliquez <a href="'.htmlspecialchars($_SERVER['HTTP_REFERER']).'">ici</a> 
+            pour revenir à la page précédente.<br />
+            Cliquez <a href="./index.php">ici</a> pour revenir à la page principale</p>';
+            echo '</div></body></html>';
+			*/					
+		?>
+
+		
+
+
+
+        
+
+	</body>
+
+</html>
